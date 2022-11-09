7ad0933c5d06049f0ed50ab5d67a32e499907955
david <dmenjim072@g.educaand.es>
2022-11-09 12:57:08 +0100

    terminado

--- src/main/java/tema4/Ejercicio4B.java
+++ src/main/java/tema4/Ejercicio4B.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package tema4;
 
 import javax.swing.JOptionPane;
@@ -11,50 +10,87 @@ import org.apache.commons.lang3.RandomStringUtils;
 public class Ejercicio4B {
 
     public static void main(String[] args) {
-        //opcion 1 - PIn 4 digitos 
-        for (int i = 0; i < 10; i++) {
+        
+        //Declaramos variables
+        String pin = "";
+        String password = "";
+        String password2 = "";
+        
+        //llamamos al metodo para el menu
+        int menuInicial = mostrarMenuInicial();
+        
+        //opcion 1 - Pin 4 digitos
+        if (menuInicial == 1) {
             
-        
-        String pin = RandomStringUtils.randomNumeric(4);
-        
-            System.out.println(pin);
+            //llamamos al metodo1
+            opcion1(pin);
+                       
         }
-        
-        System.out.println("------------------");
         
         //opcion 2 - contraseña 8 digitos
-        for (int i = 0; i < 10; i++) {
-                  
-        String password = RandomStringUtils.randomAlphanumeric(8);
-        
-            System.out.println(password);
+        if(menuInicial == 2){
+            
+            //llamamos al metodo2
+            opcion2(password);
+            
         }
-        
-        System.out.println("------------------");
-        
+
         //opcion 3 - contraseña 6 digitos con caracteres preseleccionados
-        char[] conjuntoCaracteres = {'a', 'b', 'c', 'x', '$', '€'};
-        int numero=6;
-        for (int i = 0; i < 10; i++) {
-                  
-        String password2 = RandomStringUtils.random(numero, conjuntoCaracteres);
-        
-            System.out.println(password2);
+        if(menuInicial == 3){
+            
+            //llamamos al metodo 3
+            opcion3(password2);
+            
         }
+
+    }
+
+    public static int mostrarMenuInicial() {
+
+        int menuInicial;
+
+        do {
+            String menuInicialString = JOptionPane.showInputDialog("Elija una de las 3 opciones: \n"
+                    + "1.- Crear pin para una tarjeta SIM \n"
+                    + "2.- Crear contraseña para un SO (8 carácteres) \n"
+                    + "3.- Crear contraseña eligiendo numero de carácteres");
+            menuInicial = Integer.parseInt(menuInicialString);
+        } while (menuInicial != 1 && menuInicial != 2 && menuInicial != 3);
+
+        return menuInicial;
+    }
+    
+    public static void opcion1(String pin){
+        for (int i = 0; i < 10; i++) {
+        
+            pin = RandomStringUtils.randomNumeric(4);
+            
+            System.out.println(pin);
+        }
+    }
+    
+    public static void opcion2(String password){
+        
+        for (int i = 0; i < 10; i++) {
+
+            password = RandomStringUtils.randomAlphanumeric(8);
+
+            System.out.println(password);
+        
+            }
         
     }
     
-    public static String mostrarMenuInicial(){
+    public static void opcion3(String password2){
         
-        String menuInicial;
+        char[] conjuntoCaracteres = {'a', 'b', 'c', 'x', '$', '€'};
+        int numero = 6;
+        for (int i = 0; i < 10; i++) {
+
+            password2 = RandomStringUtils.random(numero, conjuntoCaracteres);
+
+            System.out.println(password2);
+        }
         
-        do{
-        menuInicial = JOptionPane.showInputDialog("Elija una de las 3 opciones: \n"
-                + "1.- Crear pin para una tarjeta SIM \n"
-                + "2.- Crear contraseña para un SO (8 carácteres) \n"
-                + "3.- Crear contraseña eligiendo numero de carácteres");
-        }while(menuInicial==1 || menuInicial==2 || menuInicial==3);
-        
-        return menuInicial;
     }
 }
